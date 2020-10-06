507d03fefc57ecf1b827ba652b83503e6e76a4b2
kondo <kazu4126ab@gmail.com>
2020-10-07 06:21:36 +0900

    first commit

--- index.php
+++ index.php
@@ -92,4 +92,47 @@ echo '<br>';
 echo $car1->infomation();
 echo '<br>';
 
+
+class Taxi extends Car
+{
+    public $passenger;
+
+    public function infomation()
+    {
+        echo '車の車種:' . $this->name . '、' .  '車体番号:' . $this->number . '、' . 'カラー:' . $this->color . '、' . '乗車人数は' . $this->passenger . '人です。<br>';
+    }
+
+    public function pickUp($passenger)
+    {
+        $this->passenger = $passenger;
+    }
+
+    public function lower($number)
+    {
+        if ($this->passenger - $number >= 0) {
+            $this->passenger = $this->passenger - $number;
+            echo $number . '人降車しました。';
+        } else {
+            echo '降車人数に誤りがあります。';
+        }
+    }
+}
+
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
+echo '<br>';
+
+// 2人降車
+$taxi1->lower(2);
+echo '<br>';
+
+// 2人降車
+$taxi1->lower(2);
+
 ?>
