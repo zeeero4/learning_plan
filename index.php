79128b082dde24145ac215f7bed5218d1838db84
kondo <kazu4126ab@gmail.com>
2020-10-09 21:40:20 +0900

    修正

--- index.php
+++ index.php
@@ -52,51 +52,10 @@ class Car{
 
 }
 
-// 以下を実行した時にエラーがでないようにして下さい。
-// Carクラスのインスタンスを生成
-// 引数 : 車名、車体番号、カラー
-$car1 = new Car('ランクル', 100, 'black');
-
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
-
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
-
-
 class Taxi extends Car
 {
-    public $passenger;
-
+    private $passenger;
+    
     public function infomation()
     {
         echo '車の車種:' . $this->name . '、' .  '車体番号:' . $this->number . '、' . 'カラー:' . $this->color . '、' . '乗車人数は' . $this->passenger . '人です。<br>';
@@ -109,7 +68,7 @@ class Taxi extends Car
 
     public function lower($number)
     {
-        if ($this->passenger - $number >= 0) {
+        if ($this->passenger - $number >= 0){
             $this->passenger = $this->passenger - $number;
             echo $number . '人降車しました。';
         } else {
@@ -117,6 +76,12 @@ class Taxi extends Car
         }
     }
 }
+
+$car1 = new Car('ランクル', 100, 'black');
+
+echo $car1->infomation();
+echo '<br>';
+echo '<hr>';
 
 // Taxiクラスのインスタンスを生成
 $taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
